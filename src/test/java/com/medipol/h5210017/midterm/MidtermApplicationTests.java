9b7853ffad400f279b5f35b2ce1033493f5cc721
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-11-20 19:34:39 +0300

    Spring 2.7.5

---
+++ src/test/java/com/medipol/h5210017/midterm/MidtermApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medipol.h5210017.midterm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidtermApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
