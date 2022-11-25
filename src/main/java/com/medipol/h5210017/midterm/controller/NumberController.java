e2373541901bbf6625455488d088f2f8b0d11b76
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-11-25 08:01:11 +0300

    Refactor: Added @RequesMapping to NumberController

--- src/main/java/com/medipol/h5210017/midterm/controller/NumberController.java
+++ src/main/java/com/medipol/h5210017/midterm/controller/NumberController.java
@@ -6,12 +6,14 @@ import com.medipol.h5210017.midterm.service.number.NumberService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
 
 @RestController
+@RequestMapping("number")
 public class NumberController {
 
     @Autowired
