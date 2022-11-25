012de8228a2a2f99806e8abbff4282c8b60eb54a
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-11-25 08:01:29 +0300

    Add: NoArgsConstructor to BaseResponse

--- src/main/java/com/medipol/h5210017/midterm/model/BaseResponse.java
+++ src/main/java/com/medipol/h5210017/midterm/model/BaseResponse.java
@@ -2,8 +2,10 @@ package com.medipol.h5210017.midterm.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 public class BaseResponse {
     @Getter String message;
     @Getter int code;
